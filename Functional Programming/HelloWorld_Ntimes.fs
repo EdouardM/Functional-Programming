920280cb188cd0b410add6b1b668eb9eaffa98fd
Edouard Moureaux <edouard.moureaux@gmail.com>
2015-12-10 23:20:01 +0100

    Solution to Hello World N Times

---
+++ Functional Programming/HelloWorld_Ntimes.fs
@@ -0,0 +1,20 @@
+﻿//--------------------
+//Edouard Moureaux
+//December 2015
+//--------------------
+namespace FunctionalProgramming
+
+open System
+
+module Program = 
+    
+    let solution() = 
+        let n = Console.ReadLine() |> int
+        [1..n] |> List.iter (fun i -> printfn "Hello World")
+
+    //[<EntryPoint>]
+    let main argv = 
+
+        solution()
+        0
+
