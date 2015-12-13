f106764d9f8dd86be82203e26f8639555ee14277
Edouard Moureaux <edouard.moureaux@gmail.com>
2015-12-13 11:10:32 +0100

    Solution to Eval Exp x

---
+++ Functional Programming/Evaluating_Expx.fs
@@ -0,0 +1,34 @@
+﻿//--------------------
+//Edouard Moureaux
+//December 2015
+//--------------------
+namespace FunctionalProgramming
+
+
+module Solution =
+    open System
+
+    /// Power
+    let pow x n = x ** n
+
+    /// Factorial
+    let rec fact n = match n with
+                        | n when n <= 1.0 -> 1.0
+                        | n -> n * fact (n - 1.0)
+
+    /// Exponential expension
+    let exp x = 
+        [0.0 .. 9.0] 
+        |> List.map(fun i -> (pow x i)  / (fact i) )
+        |> List.fold(fun acc elem -> acc + elem) 0.0
+
+
+    //[<EntryPoint>]
+    let main argv = 
+        let N = Console.ReadLine() |> int
+        [1..N] 
+        |> List.map(fun i -> Console.ReadLine() |> float)
+        |> List.map(fun x -> exp x)
+        |> List.iter (printfn "%.4f")
+        0
+    
