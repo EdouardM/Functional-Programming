1151ecfc965661f52a9c49ee2882eae03e7a2133
Edouard Moureaux <edouard.moureaux@gmail.com>
2015-12-13 11:38:41 +0100

    Solution to Computing GCD

---
+++ Functional Programming/Computing_GCD.fs
@@ -0,0 +1,24 @@
+﻿//--------------------
+//Edouard Moureaux
+//December 2015
+//
+// Problem URL: 
+// https://www.hackerrank.com/challenges/functional-programming-warmups-in-recursion---gcd
+//--------------------
+namespace FunctionalProgramming.Recursion
+
+module Computing_GCD =
+    open System 
+
+    /// Greatest common divisor
+    let rec gcd x y = 
+        if x = y then x
+        elif x > y then gcd (x-y) y
+        else gcd x (y - x)
+
+    //[<EntryPoint>]
+    let main argv =
+        //input 2 integers separated by space
+        let [|x; y|]= Console.ReadLine().Split[|' '|] |> Array.map int64
+        printfn "%d" (gcd x y)
+        0
