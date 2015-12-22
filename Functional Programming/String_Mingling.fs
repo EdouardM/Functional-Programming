0f2120779e59efdae2688619507885a6742c4e46
Edouard Moureaux <edouard.moureaux@gmail.com>
2015-12-22 08:06:53 +0100

    Solution to String Mingling

--- Functional Programming/String_Mingling.fs
+++ Functional Programming/String_Mingling.fs
@@ -10,7 +10,26 @@ namespace FunctionalProgramming.Recursion
 module String_Mingling = 
     open System
 
-    let mingling (p:string) (q:string) =
+    ///Permutre items in a list: Recursive
+    let mingling l1 l2 = 
+        let rec loop acc l1 l2 = 
+            match l1 , l2 with
+                | x::xs , y::ys -> 
+                        let newacc = (string y)::(string x)::acc
+                        loop newacc xs ys 
+                | [], [] -> acc |> List.rev
+                | _ , [] -> acc |> List.rev
+                | [] , _ -> acc |> List.rev
+        loop [] l1 l2
+    
+    let solution (a:string) (b:string) =
+        let l1 = a |> List.ofSeq
+        let l2 = b |> List.ofSeq
+        mingling l1 l2
+        |> String.concat ""
+
+    ///Fold Solution: too slow: Need String concat optimization
+    let mingling' (p:string) (q:string) =
         let arr1 = p |> Seq.toArray
         let arr2 = q |> Seq.toArray
         
@@ -20,5 +39,5 @@ module String_Mingling =
     let main argv = 
         let p = Console.ReadLine()
         let q = Console.ReadLine()
-        mingling p q |> printfn "%s"
+        solution p q |> printfn "%s"
         0
