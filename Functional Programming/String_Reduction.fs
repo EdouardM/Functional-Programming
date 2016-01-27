bdf20c2b901f469e1cd3e09bc65f8bec727a26a6
Edouard Moureaux <edouard.moureaux@gmail.com>
2016-01-27 22:23:14 +0100

    Solution to String Reduction

---
+++ Functional Programming/String_Reduction.fs
@@ -0,0 +1,35 @@
+﻿//--------------------
+//Edouard Moureaux
+//January 2016
+//
+// Problem URL: 
+// https://www.hackerrank.com/challenges/string-reductions
+//--------------------
+namespace FunctionalProgramming.Recursion
+
+module String_Compression =
+    open System
+
+    ///Add count to char if equals last char in compr list
+    ///Else add char with count 1
+    let reduction (str :string)= 
+        str.ToCharArray()
+        |> Array.fold(fun (acc: Map<char,int>) char -> 
+                                if acc.ContainsKey(char) then
+                                    acc
+                                else
+                                    let pos = acc.Count
+                                    acc.Add(char ,pos)
+                                ) Map.empty
+        |> Map.toArray
+        |> Array.sortBy(fun (_, pos) -> pos)
+        |> Array.map (fun (char, _) -> string char)
+        |> String.concat ""
+
+    //[<EntryPoint>]
+    let main argv =
+        Console.ReadLine()
+        |> reduction
+        |> printfn "%s"
+        |> ignore
+        0
